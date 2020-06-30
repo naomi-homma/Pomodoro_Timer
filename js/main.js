c9f732843d54c2c4ebcf9ebc81f129a433fe5ffc
naomi_homma <hnm.n.1029@gmail.com>
2020-06-30 17:55:55 +0900

    RESET機能実装、コメント記入

--- js/main.js
+++ js/main.js
@@ -8,43 +8,55 @@
   const timeState = document.getElementById('timeState');
   const cycle = document.getElementById('cycle');
  
+  // workTime、breakTime、longBreakTime、cycleCountは後々ユーザーが入力できるようにしたい
+  // workTime：作業時間
+  // breakTime：小休憩時間
+  // longBreakTime：長休憩時間
+  // cycleCountt：（作業+小休憩）のサイクル回数
+
   const workTime = 0.1;
   const breakTime = 0.2;
   const longBreakTime = 0.3;
+
+  // 状態をstateで管理する：work,break,longBreak
   let state = "work";
   let startTime;
   let workTimeoutId;
   let breakTimeoutId;
   let longBreakTimeoutId;
   let elapsedTime = 0;
-  let initCount = 3;
+  let cycleCount = 3; 
   let count = 1;
 
   // 最初のレンダリング後の描画
   timer.textContent = `${String(workTime).padStart(2, '0')}:00`;
   timeState.textContent = state;
-  cycle.textContent = `1/${initCount}`;
+  cycle.textContent = `1/${cycleCount}`;
 
-  // ミリ秒で残り時間を計算する
-  // workTimeCountDown関数⇒残り時間の計算、ミリ秒から(分、秒)への変換、描画を行う
-  // 残り時間が0になったらカウントダウンを止めて、breakTimeCountDownを呼び出す
+  // workTimeCountDown()、breakTimeCountDown()、longBreakTimeCountDown()を
+  // 各ターン終了後に呼び出す
+
   function workTimeCountDown() {
     state = "work";
+    // 残り時間をミリ秒で計算⇒分・秒に変換⇒描画
     const remainigTime = (workTime * 60 *1000) - elapsedTime - (Date.now() - startTime);
     const restMin = String(Math.floor((remainigTime / 1000 / 60) % 60)).padStart(2, '0');
     const restSec = String(Math.floor((remainigTime / 1000) % 60)).padStart(2, '0');
     timer.textContent = `${restMin}:${restSec}`;
+    // ●0になるまではカウントダウンを繰り返す
     if ( remainigTime > 0 ) {
       workTimeoutId = setTimeout(() => {
         workTimeCountDown();
         }, 100); 
-      } else if ( remainigTime < 0 && count === 3 ) {
+      // ●指定回数のサイクルが終了したら長休憩に入る
+      } else if ( remainigTime < 0 && count === cycleCount ) {
         clearTimeout(workTimeoutId);
         startTime = Date.now();
         elapsedTime = 0;
         longBreakTimeCountDown();
         timeState.textContent = state;
       } else {
+      // ●指定回数のサイクル未満は小休憩に入る
       clearTimeout(workTimeoutId);
       startTime = Date.now();
       elapsedTime = 0;
@@ -65,11 +77,12 @@
         breakTimeCountDown();
         }, 100); 
       } else {
+      // ●小休憩に入ったらcountを増やして状態を管理する
       clearTimeout(breakTimeoutId);
       startTime = Date.now();
       elapsedTime = 0;
       count ++;
-      cycle.textContent = `${count}/${initCount}`;
+      cycle.textContent = `${count}/${cycleCount}`;
       workTimeCountDown();
       timeState.textContent = state;
     }
@@ -87,11 +100,12 @@
         longBreakTimeCountDown();
         }, 100); 
       } else {
+      // ●長い休憩が終了したらcountを1に戻し最初へ戻る
       clearTimeout(longBreakTimeoutId);
       startTime = Date.now();
       elapsedTime = 0;
       count = 1;
-      cycle.textContent = `${count}/${initCount}`;
+      cycle.textContent = `${count}/${cycleCount}`;
       workTimeCountDown();
       timeState.textContent = state;
     }
@@ -122,7 +136,14 @@
   });
 
   reset.addEventListener('click', () => {
-    timer.textContent = `${String(workTime).padStart(2, '0')}:00`;
+    switch (state) {
+      case "work": timer.textContent = `${String(workTime).padStart(2, '0')}:00`;
+        break;
+      case "break": timer.textContent = `${String(breakTime).padStart(2, '0')}:00`;
+        break;
+      case "longBreak": timer.textContent = `${String(longBreakTime).padStart(2, '0')}:00`;
+        break;
+    }
     elapsedTime = 0;
   });
 }
