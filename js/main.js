ba57deeb58827e72807459a6aee9398e3de63548
naomi_homma <hnm.n.1029@gmail.com>
2020-07-17 12:39:44 +0900

    音源ファイルパス修正、カウントダウン関数内if条件追加

--- js/main.js
+++ js/main.js
@@ -81,8 +81,8 @@
         state = "longBreak";
         displayState(state);
         countDown(longBreakTime);
-      } else if (state === "work") {
-        audio.src = "../assets/audio/hatoclock.mp3#t=0,3.5";
+      } else if (count < cycleCount && state === "work") {
+        audio.src = "https://naomi-homma.github.io/Pomodoro_Timer/assets/audio/hatoclock.mp3#t=0,3.5";
         const playPromise = audio.play();
         if(playPromise !== undefined) {
           playPromise.then(_ => {
